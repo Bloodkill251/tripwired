f4867e32f2df6d748d4f9fffb9362b3c56904100
Bloodkill251 <Blood@192.168.1.23>
2021-01-07 15:21:52 +0000

    this-is-annoying

---
+++ src/main/java/net/mcreator/tripwired/procedures/GotoVoidCommandExecutedProcedure.java
@@ -0,0 +1,51 @@
+package net.mcreator.tripwired.procedures;
+
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
+import net.minecraft.network.play.server.SPlaySoundEventPacket;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.tripwired.world.dimension.VoidDimensionDimension;
+import net.mcreator.tripwired.TripwiredModElements;
+
+import java.util.Map;
+
+@TripwiredModElements.ModElement.Tag
+public class GotoVoidCommandExecutedProcedure extends TripwiredModElements.ModElement {
+	public GotoVoidCommandExecutedProcedure(TripwiredModElements instance) {
+		super(instance, 133);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure GotoVoidCommandExecuted!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+				DimensionType destinationType = VoidDimensionDimension.type;
+				ObfuscationReflectionHelper.setPrivateValue(ServerPlayerEntity.class, (ServerPlayerEntity) _ent, true, "field_184851_cj");
+				ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+				((ServerPlayerEntity) _ent).connection.sendPacket(new SChangeGameStatePacket(4, 0));
+				((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), nextWorld.getSpawnPoint().getY() + 1,
+						nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+				((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+				for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+				}
+				((ServerPlayerEntity) _ent).connection.sendPacket(new SPlaySoundEventPacket(1032, BlockPos.ZERO, 0, false));
+			}
+		}
+	}
+}
